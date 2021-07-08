243b6fae50d62a02d24754b6c94e308e4c385042
Dean Pinlac <dean.pinlac@gmail.com>
2021-07-07 22:50:09 -0400

    Added Main Activity - empty

---
+++ app/src/main/java/com/centennial/deanpinlac_sanjibsaha_comp304sec002/MainActivity.java
@@ -0,0 +1,14 @@
+package com.centennial.deanpinlac_sanjibsaha_comp304sec002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
